eaab2148ba309c5d0ee9bbcb742cb538f4ef3c3d
fatemehmaghsoodi <fatemehhmaghsoodi@gmail.com>
2025-07-27 10:56:20 +0330

    feat: add dev server proxy config for API requests

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [react()],
   // base: '/',
    server:{
-    port:3000
+    port:3000,
+    proxy: {
+      '/api': {
+        target: 'https://foodapp-backend-production-d265.up.railway.app',
+        changeOrigin: true,
+      }
+    }
    }
 })
